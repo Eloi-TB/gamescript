2bb8a5053e5a46b5273fbefbf5326e46222f0eda
Eloi Trepat <eloi.trepat@between.tech>
2017-05-16 20:46:14 +0200

    Columna Scores a la taula Games y Model de User

---
+++ database/migrations/2017_05_16_183156_add_column_to_scores.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToScores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            $table->integer('score')->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
