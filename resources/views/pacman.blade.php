81db24015d59757563dce2967c336de5be0d50a1
Eloi Trepat <eloi.trepat@between.tech>
2017-05-25 19:29:08 +0200

    sonidos pacman

--- resources/views/pacman.blade.php
+++ resources/views/pacman.blade.php
@@ -18,13 +18,7 @@
     </div>
     <!-- Div de controles del juego -->
     <div class="col-md-3">
-    </br></br>
-    Controles:<br><br>
-    <table style="text-align:center;">
-      <tr><td></td><td>Subir</td><td></td></tr>
-      <tr><td>Izquierda</td><td><IMG SRC="./img/flechas.png" width="100" height="80"></td><td>Derecha</td></tr>
-        <tr><td></td><td>Bajar</td><td></td></tr>
-      </table>
+    @include('controls.type1')
     </div>
     <?php
     /* Guarda en variable php el nombre del juego */
@@ -33,8 +27,9 @@
     <!-- Div en el que se muestra el juego -->
     <div class="col-md-5">
       <div id="pacman"></div>
-      <script src="pacman.js"></script>
-      <script src="modernizr-1.5.min.js"></script>
+
+      <script src={{ asset('js/games/pacman/pacman.js') }}></script>
+      <script src={{ asset('js/games/pacman/modernizr-1.5.min.js') }}></script>
       <script>
       var el = document.getElementById("pacman");
 
@@ -60,12 +55,12 @@
   </div>
   <div class="row">
     <div class="col-md-4">
-      <!----- Boton de volver ---->
+      <!-- Boton de volver
       <form action="../../juegos.php" method="post" name="usuario">
-        <input type="hidden" name="nombre" value="<?php echo htmlspecialchars("{{ Auth::user()->name }}"); ?>">
+        <input type="hidden" name="nombre" value="<php echo htmlspecialchars("{ Auth::user()->name }}"); ?>">
         <button type="submit" class="button" >Volver</button>
       </form>
-    </div>
+    </div> -->
     <!----- Div de publicidad ---->
     <div class="col-md-4">
       <img src="../img/pollo.jpg" id="publi" width="500" height="100">
@@ -73,7 +68,8 @@
     <div class="col-md-4"> </div>
   </div>
 </div>
-<script src="../js/jquery-2.1.4.min.js"></script>
-                    <script src="../js/bootstrap.min.js"></script>
+<!--
+<script src={{ asset('js/jquery-2.1.4.min.js') }}></script>
+<script src={{ asset('js/bootstrap.min.js') }}></script> -->
 <!-- include('scripts-pacman') -->
 @endsection
