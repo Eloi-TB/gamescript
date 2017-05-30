f8c44067749e388986765f00b56e18d3c31ec30f
Eloi Trepat <eloi.trepat@between.tech>
2017-05-30 19:26:20 +0200

    controles

---
+++ resources/views/controls/space.php
@@ -0,0 +1,36 @@
+<br />
+<br />
+Controles:
+<br />
+<br />
+<table style="text-align:center;">
+  <tr>
+    <td>
+    </td>
+    <td>
+      Subir
+    </td>
+    <td>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      Izquierda
+    </td>
+    <td>
+      <img src={{ asset('img/flechas.png') }} width="100" height="80">
+    </td>
+    <td>
+      Derecha
+    </td>
+  </tr>
+  <tr>
+    <td>
+    </td>
+    <td>
+      Bajar
+    </td>
+    <td>
+    </td>
+  </tr>
+</table>
